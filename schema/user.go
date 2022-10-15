1d5bb7882e08820acac5d5d98ea21a65ec54c0e7
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-10-16 01:31:46 +0700

    chore : delete schema

--- schema/user.go
+++
@@ -1,9 +0,0 @@
-package schema
-
-type UserSchema struct {
-	UserId       int    `json:"user_id"`
-	UserName     string `json:"user_name"`
-	UserEmail    string `json:"user_email"`
-	UserRole     string `json:"user_role"`
-	UserPassword string `json:"user_password,omitempty"`
-}

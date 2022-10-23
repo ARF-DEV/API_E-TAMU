4605fc3b35acd080b0cf5870e587e0f601505dd6
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-10-23 16:27:23 +0700

    chore : delete feedback

--- models/visit.go
+++ models/visit.go
@@ -1,16 +1,15 @@
 package models
 
 type Visit struct {
-	VisitId            int    `db:"visit_id"`
-	UserVisitedId      int    `db:"user_visited_id"`
-	GuestName          string `db:"guest_name"`
-	GuestEmail         string `db:"guest_email"`
-	VisitIntention     string `db:"visit_intention"`
-	VaccineCertificate string `db:"vaccine_certificate"`
-	VisitStatus        string `db:"visit_is_done"`
-	GuestFeedback      string `db:"guest_feedback"`
-	GuestCount         int    `db:"guest_count"`
-	VisitDate          string `db:"visit_date"`
-	VisitHour          string `db:"visit_hour"`
-	Transportation     string `db:"transpotation"`
+	VisitId            int    `db:"visit_id" json:"visit_id"`
+	UserVisitedId      int    `db:"user_visited_id" json:"user_visited_id"`
+	GuestName          string `db:"guest_name" json:"guest_name"`
+	GuestEmail         string `db:"guest_email" json:"guest_email"`
+	VisitIntention     string `db:"visit_intention" json:"visit_intention"`
+	VaccineCertificate string `db:"vaccine_certificate" json:"vaccine_certificate"`
+	VisitStatus        string `db:"visit_status" json:"visit_status"`
+	GuestCount         int    `db:"guest_count" json:"guest_count"`
+	VisitDate          string `db:"visit_date" json:"visit_date"`
+	VisitHour          string `db:"visit_hour" json:"visit_hour"`
+	Transportation     string `db:"transportation" json:"transportation"`
 }

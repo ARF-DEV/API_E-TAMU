cd4cfbb6f31192ea759d6c6baf09b06a071d94a6
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-10-23 17:33:22 +0700

    chore : add validate tag to models

--- models/user.go
+++ models/user.go
@@ -1,9 +1,9 @@
 package models
 
 type User struct {
-	UserId       int    `db:"user_id" json:"user_id"`
-	UserName     string `db:"user_name" json:"user_name"`
-	UserEmail    string `db:"user_email" json:"user_email"`
-	UserRole     string `db:"user_role" json:"user_role"`
-	UserPassword string `db:"user_password" json:"user_password"`
+	UserId       int    `db:"user_id" json:"user_id" validate:"required"`
+	UserName     string `db:"user_name" json:"user_name" validate:"required"`
+	UserEmail    string `db:"user_email" json:"user_email" validate:"required,email"`
+	UserRole     string `db:"user_role" json:"user_role" validate:"required"`
+	UserPassword string `db:"user_password" json:"user_password" validate:"required"`
 }
--- models/visit.go
+++ models/visit.go
@@ -1,15 +1,15 @@
 package models
 
 type Visit struct {
-	VisitId            int    `db:"visit_id" json:"visit_id"`
-	UserVisitedId      int    `db:"user_visited_id" json:"user_visited_id"`
-	GuestName          string `db:"guest_name" json:"guest_name"`
-	GuestEmail         string `db:"guest_email" json:"guest_email"`
-	VisitIntention     string `db:"visit_intention" json:"visit_intention"`
-	VaccineCertificate string `db:"vaccine_certificate" json:"vaccine_certificate"`
-	VisitStatus        string `db:"visit_status" json:"visit_status"`
-	GuestCount         int    `db:"guest_count" json:"guest_count"`
-	VisitDate          string `db:"visit_date" json:"visit_date"`
-	VisitHour          string `db:"visit_hour" json:"visit_hour"`
-	Transportation     string `db:"transportation" json:"transportation"`
+	VisitId            int    `db:"visit_id" json:"visit_id" validate:"required"`
+	UserVisitedId      int    `db:"user_visited_id" json:"user_visited_id" validate:"required"`
+	GuestName          string `db:"guest_name" json:"guest_name" validate:"required"`
+	GuestEmail         string `db:"guest_email" json:"guest_email" validate:"required"`
+	VisitIntention     string `db:"visit_intention" json:"visit_intention" validate:"required"`
+	VaccineCertificate string `db:"vaccine_certificate" json:"vaccine_certificate" validate:"required"`
+	VisitStatus        string `db:"visit_status" json:"visit_status" validate:"required"`
+	GuestCount         int    `db:"guest_count" json:"guest_count" validate:"required"`
+	VisitDate          string `db:"visit_date" json:"visit_date" validate:"required"`
+	VisitHour          string `db:"visit_hour" json:"visit_hour" validate:"required"`
+	Transportation     string `db:"transportation" json:"transportation" validate:"required"`
 }

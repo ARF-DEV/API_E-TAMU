b3962cd0874907eefc8787e5b76dd868c98ad2bd
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-10-23 17:33:52 +0700

    chore : impelement visit services

--- services/visit.go
+++ services/visit.go
@@ -2,13 +2,96 @@ package services
 
 import (
 	"E-TamuAPI/helpers"
+	"E-TamuAPI/models"
 	"E-TamuAPI/repository"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/go-chi/chi/v5"
 )
+
+type DateRange struct {
+	StartDate string `json:"start_date" validate:"required"`
+	EndDate   string `json:"end_date" validate:"required"`
+}
+
+func GetVisitByID(visitRepo *repository.VisitRepository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var v models.Visit
+
+		defer r.Body.Close()
+		err := json.NewDecoder(r.Body).Decode(&v)
+
+		if err != nil {
+			fmt.Println("Error while getting visit by id: ", err.Error())
+			helpers.ErrorResponseJSON(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+
+		visit, err := visitRepo.GetVisitByID(v.VisitId)
+
+		if err != nil {
+			fmt.Println("Error while getting visit by id: ", err.Error())
+			helpers.ErrorResponseJSON(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		helpers.SuccessResponseJSON(w, "Success", visit)
+	}
+}
+
+func GetVisitByStaffID(visitRepo *repository.VisitRepository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		var u models.User
+
+		defer r.Body.Close()
+		err := json.NewDecoder(r.Body).Decode(&u)
+		if err != nil {
+			log.Println("Error on visits by Staff ID: ", err.Error())
+			helpers.ErrorResponseJSON(w, "Json is Invalid", http.StatusBadRequest)
+			return
+		}
+
+		visits, err := visitRepo.GetVisitByStaffID(u.UserId)
+
+		if err != nil {
+			fmt.Println("Error while getting visits by staff id: ", err.Error())
+			helpers.ErrorResponseJSON(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		helpers.SuccessResponseJSON(w, "Success", visits)
+	}
+}
+
+func GetVisitsByDateRange(visitRepo *repository.VisitRepository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var d DateRange
+
+		defer r.Body.Close()
+
+		err := json.NewDecoder(r.Body).Decode(&d)
+
+		if err != nil {
+			log.Println("Error on visits by date range: ", err.Error())
+			helpers.ErrorResponseJSON(w, "Json is Invalid", http.StatusBadRequest)
+			return
+		}
+
+		visits, err := visitRepo.GetVisitByDate(d.StartDate, d.EndDate)
+
+		if err != nil {
+			fmt.Println("Error while getting visits by date range: ", err.Error())
+			helpers.ErrorResponseJSON(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		helpers.SuccessResponseJSON(w, "Success", visits)
+	}
+}
 
 func GetAllVisit(visitRepo *repository.VisitRepository) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {

5bc21691674033418f777c33af699b3506ccc137
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-10-23 16:27:06 +0700

    chore : delete user

--- models/user.go
+++ models/user.go
@@ -1,21 +1,9 @@
 package models
 
-import "E-TamuAPI/schema"
-
 type User struct {
-	UserId       int    `db:"user_id"`
-	UserName     string `db:"user_name"`
-	UserEmail    string `db:"user_email"`
-	UserRole     string `db:"user_role"`
-	UserPassword string `db:"user_password"`
-}
-
-func (a *User) GetSchema() schema.UserSchema {
-	return schema.UserSchema{
-		UserId:       a.UserId,
-		UserName:     a.UserName,
-		UserEmail:    a.UserEmail,
-		UserRole:     a.UserRole,
-		UserPassword: a.UserPassword,
-	}
+	UserId       int    `db:"user_id" json:"user_id"`
+	UserName     string `db:"user_name" json:"user_name"`
+	UserEmail    string `db:"user_email" json:"user_email"`
+	UserRole     string `db:"user_role" json:"user_role"`
+	UserPassword string `db:"user_password" json:"user_password"`
 }
